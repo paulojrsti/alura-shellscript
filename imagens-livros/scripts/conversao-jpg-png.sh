fe4013a388eb1607c6186f11e87c93b6993c785e
Paulo Jacinto <paulo.jacinto@enesolucoes.com.br>
2022-02-14 17:01:24 -0300

    inserçao de if para saber se a conversão deu certo

--- imagens-livros/scripts/conversao-jpg-png.sh
+++ imagens-livros/scripts/conversao-jpg-png.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 converteImagem() {
-cd /Users/paulojacinto/Documents/cursoshell/imagens-livros
+cd /Users/paulojacinto/Documents/cursoshell/imagens-livrosfdfsa
     if [ ! -d png ]
     then
         mkdir png
@@ -10,17 +10,19 @@ cd /Users/paulojacinto/Documents/cursoshell/imagens-livros
     for imagem in *.jpg
     do
         local imagem_sem_extensao=$(ls $imagem | awk -F . '{ print $1 }')
+        echo $imagem_sem_extensao
         sips -s format png $imagem_sem_extensao.jpg --out png/$imagem_sem_extensao.png
     done
 }
 
 converteImagem 2> erros_conversao.txt
+RESULTADO=$?
 
-if [ $? -eq 0 ]
+if [ $RESULTADO -eq 0 ]
 then 
     echo "Conversão realizada com sucesso"
 else
     echo "Houve uma falha no processo"
 fi
 
-echo "Saida: " $?
+echo "Saida do Resultado: " $RESULTADO
