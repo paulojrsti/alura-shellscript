a874f650279e5aeaea586f4feec2158b579c0f48
Paulo Jacinto <paulo.jacinto@enesolucoes.com.br>
2022-02-13 21:49:14 -0300

    Criação de arquivo de erros com saída 2

--- imagens-livros/scripts/conversao-jpg-png.sh
+++ imagens-livros/scripts/conversao-jpg-png.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+converteImagem() {
 cd /Users/paulojacinto/Documents/cursoshell/imagens-livros
-if [ ! -d png ]
-then
-    mkdir png
+    if [ ! -d png ]
+    then
+        mkdir png
+    fi
+
+    for imagem in *.jpg
+    do
+        local imagem_sem_extensao=$(ls $imagem | awk -F . '{ print $1 }')
+        sips -s format png $imagem_sem_extensao.jpg --out png/$imagem_sem_extensao.png
+    done
+}
+
+converteImagem 2> erros_conversao.txt
+
+if [ $? -eq 0 ]
+then 
+    echo "Conversão realizada com sucesso"
+else
+    echo "Houve uma falha no processo"
 fi
 
-for imagem in *.jpg
-do
-    imagem_sem_extensao=$(ls $imagem | awk -F . '{ print $1 }')
-    sips -s format png $imagem_sem_extensao.jpg --out png/$imagem_sem_extensao.png
-done
-    
-
+echo "Saida: " $?
